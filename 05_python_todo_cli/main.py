97ee97ae3821b676d105b593901d44fb0a2b9b7f
WilliamLoyd <info@twconsulting.biz>
2025-12-10 22:15:52 +0000

    Day 5: Begin first attempt at indepedent work

---
+++ 05_python_todo_cli/main.py
@@ -0,0 +1,9 @@
+
+tasks = []
+
+def take_tasks():
+    task_1 = input("Please input your first task:\n")
+    tasks.append(task_1)
+    print(f"{task_1} has been added to the list!")
+
+take_tasks()
